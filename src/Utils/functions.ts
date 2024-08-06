115bb8a528cb52f4af0dee175d47f13cdd45fb97
mahmoudhamed169 <mh4190207@gmail.com>
2024-08-07 01:14:18 +0300

    JSDOC ( Clean Code )

--- src/Utils/functions.ts
+++ src/Utils/functions.ts
@@ -1,3 +1,9 @@
+/**
+ *
+ * @param {string} txt -The input text to be slices
+ * @param {number} (max = 50) - the max length
+ * @returns -the sliced text with an ...
+ */
 export function txtSlicer(txt: string, max: number = 50) {
   if (txt.length >= max) return `${txt.slice(0, max)} ...`;
   else txt;
